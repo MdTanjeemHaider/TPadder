bde4ce0faab805f3cb472f538d011a2ddb0140f5
MdTanjeemHaider <mdtanjeem.haider@ontariotechu.net>
2025-11-12 20:05:04 -0500

    Update build script and .gitignore

--- Build.bat
+++ Build.bat
@@ -1,1 +1,13 @@
-pyinstaller --clean --onefile --noconsole --workpath build --specpath build --distpath build --icon=../Icons/icon.ico --add-data "../Icons/icon.ico;Icons" TPadder.py
+@echo off
+REM Build script for TPadder
+pyinstaller ^
+    --clean ^
+    --onefile ^
+    --noconsole ^
+    --workpath build ^
+    --specpath build ^
+    --distpath build ^
+    --icon=../Icons/icon.ico ^
+    --add-data "../Icons/icon.ico;Icons" ^
+    main.py
+echo Build complete. Executable is located in the build directory.
